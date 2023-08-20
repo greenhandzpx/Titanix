5f2cff71e068680791896f08b4b1c5fddd8ba562
ghj1222 <1342330485@qq.com>
2023-08-20 12:06:32 +0800

    fix: use static array instead of vector

--- kernel/src/irq_count.rs
+++ kernel/src/irq_count.rs
@@ -1,26 +1,21 @@
 use crate::sync::mutex::SpinNoIrqLock;
 use alloc::string::{String, ToString};
-use alloc::vec::Vec;
 
 type Mutex<T> = SpinNoIrqLock<T>;
 
 pub struct IrqCounter {
-    array: Mutex<Vec<usize>>,
+    array: Mutex<[usize; 100]>,
 }
 
 impl IrqCounter {
     pub const fn new() -> Self {
         Self {
-            array: Mutex::new(Vec::new()),
+            array: Mutex::new([0; 100]),
         }
     }
     pub fn add1(&self, id: usize) {
+        //        println!("add1 {}", id);
         let mut array = self.array.lock();
-        if array.len() <= id {
-            for _ in 0..=(id - array.len()) {
-                array.push(0);
-            }
-        }
         array[id] += 1;
     }
     pub fn dump(&self) -> String {
